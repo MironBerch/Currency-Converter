a63b82b6778e0b41b8db7f093b0e5424fa96f5c5
MironBerch <106734953+MironBerch@users.noreply.github.com>
2022-07-01 22:49:53 +0300

    conversion

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+"""cc URL Configuration"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('conversion.urls'))
+]
