5eb0478fe0f06f9d62bc08e9543ebd2b03df7a6f
MironBerch <106734953+MironBerch@users.noreply.github.com>
2022-07-14 11:57:16 +0300

    Delete config directory

--- config/urls.py
+++
@@ -1,8 +0,0 @@
-"""cc URL Configuration"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('conversion.urls'))
-]
